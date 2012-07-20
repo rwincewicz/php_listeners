3eb441ada3ccacc99161bf421cface1b6dec58a6
rwincewicz <richard.wincewicz@googlemail.com>
2012-07-20 15:03:44 -0300

    Changed how Stomp connection is set up

--- Derivatives.php
+++ Derivatives.php
@@ -201,7 +201,8 @@ class Derivative {
       $output_file = '/opt/php_listeners/document-embargo.xml';
       $this->add_derivative($dsid, $label, $output_file, 'text/xml', FALSE);
       $datastream = new FedoraDatastream($dsid, $this->object, $this->fedora_object->repository);
-      $datastream->label = $label;
+      $datastream->state = 'I';
+      $datastream->state = 'A';
     } catch (Exception $e) {
       $this->log->lwrite("Could not create the $dsid derivative!", 'FAIL_DATASTREAM', $this->pid, $dsid, NULL, 'ERROR');
     }
